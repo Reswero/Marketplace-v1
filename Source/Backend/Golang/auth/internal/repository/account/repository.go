b7fe25a9f71288667c2054d7f6288d39ba5b8327
Reswero <reswero@mail.ru>
2024-02-03 01:00:40 +0600

    Добавлено получение аккаунта по номеру телефона в репозиторий (Auth)

--- Source/Backend/Golang/auth/internal/repository/account/repository.go
+++ Source/Backend/Golang/auth/internal/repository/account/repository.go
@@ -12,6 +12,10 @@ import (
 	"github.com/Reswero/Marketplace-v1/pkg/postgres"
 )
 
+const (
+	accountsTable = "accounts"
+)
+
 // Репозиторий аккаунтов
 type Repository struct {
 	*postgres.Storage
@@ -27,7 +31,7 @@ func New(storage *postgres.Storage) *Repository {
 func (r *Repository) CreateAccount(ctx context.Context, acc *account.Account) (int, error) {
 	const op = "repository.account.CreateAccount"
 
-	sql, args, err := r.Builder.Insert("accounts").
+	sql, args, err := r.Builder.Insert(accountsTable).
 		Columns("type", "phone_number", "email",
 			"created_at", "password", "salt").
 		Values(acc.Type, acc.PhoneNumber, acc.Email,
@@ -51,21 +55,36 @@ func (r *Repository) GetAccount(ctx context.Context, id int) (*account.Account, 
 	const op = "repository.account.GetAccount"
 
 	sql, args, err := r.Builder.Select("*").
-		From("accounts").
+		From(accountsTable).
 		Where(squirrel.Eq{"id": id}).
 		ToSql()
 	if err != nil {
 		return nil, formatter.FmtError(op, err)
 	}
 
-	acc := &account.Account{}
-	err = r.Pool.QueryRow(ctx, sql, args...).
-		Scan(&acc.Id, &acc.Type, &acc.PhoneNumber,
-			&acc.Email, &acc.CreatedAt, &acc.Password, &acc.Salt)
+	acc, err := r.getAccountFromQuery(ctx, sql, args...)
 	if err != nil {
-		if errors.Is(err, sqldb.ErrNoRows) {
-			return nil, err
-		}
+		return nil, formatter.FmtError(op, err)
+	}
+
+	return acc, nil
+}
+
+// Получение аккаунта по номеру телефона и типу
+func (r *Repository) GetAccountByPhoneNumber(ctx context.Context, phoneNumber string,
+	accType account.AccountType) (*account.Account, error) {
+	const op = "repository.account.GetAccountByPhoneNumber"
+
+	sql, args, err := r.Builder.Select("*").
+		From(accountsTable).
+		Where(squirrel.Eq{"phone_number": phoneNumber, "type": accType}).
+		ToSql()
+	if err != nil {
+		return nil, formatter.FmtError(op, err)
+	}
+
+	acc, err := r.getAccountFromQuery(ctx, sql, args...)
+	if err != nil {
 		return nil, formatter.FmtError(op, err)
 	}
 
@@ -76,7 +95,7 @@ func (r *Repository) GetAccount(ctx context.Context, id int) (*account.Account, 
 func (r *Repository) UpdateAccount(ctx context.Context, acc *account.Account) error {
 	const op = "repository.account.UpdateAccount"
 
-	sql, args, err := r.Builder.Update("accounts").
+	sql, args, err := r.Builder.Update(accountsTable).
 		Set("phone_number", acc.PhoneNumber).
 		Set("email", acc.Email).
 		Set("password", acc.Password).
@@ -99,7 +118,7 @@ func (r *Repository) UpdateAccount(ctx context.Context, acc *account.Account) er
 func (r *Repository) DeleteAccount(ctx context.Context, acc *account.Account) error {
 	const op = "repository.account.DeleteAccount"
 
-	sql, args, err := r.Builder.Delete("accounts").
+	sql, args, err := r.Builder.Delete(accountsTable).
 		Where(squirrel.Eq{"id": acc.Id}).
 		ToSql()
 	if err != nil {
@@ -112,4 +131,21 @@ func (r *Repository) DeleteAccount(ctx context.Context, acc *account.Account) er
 	}
 
 	return nil
+}
+
+func (r *Repository) getAccountFromQuery(ctx context.Context, sql string, args ...interface{}) (*account.Account, error) {
+	const op = "repository.account.getAccountFromQuery"
+
+	acc := &account.Account{}
+	err := r.Pool.QueryRow(ctx, sql, args...).Scan(&acc.Id, &acc.Type, &acc.PhoneNumber,
+		&acc.Email, &acc.CreatedAt, &acc.Password, &acc.Salt)
+	if err != nil {
+		if errors.Is(err, sqldb.ErrNoRows) {
+			return nil, err
+		}
+
+		return nil, formatter.FmtError(op, err)
+	}
+
+	return acc, nil
 }
