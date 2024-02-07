d45a19424552d5da02840d452ab235b4e49f4082
Reswero <reswero@mail.ru>
2024-02-07 22:07:20 +0600

    Добавлен запрос на получение покупателя

---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Application/Customers/Queries/GetCustomer/GetCustomerQuery.cs
@@ -0,0 +1,6 @@
+﻿using Marketplace.Users.Domain;
+using MediatR;
+
+namespace Marketplace.Users.Application.Customers.Queries.GetCustomer;
+
+public record GetCustomerQuery(int AccountId) : IRequest<Customer>;
