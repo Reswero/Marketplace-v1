ce358b33b8eb138c0fe0fcdb23ad84f5f5b88603
Reswero <reswero@mail.ru>
2024-02-07 21:57:11 +0600

    Добавлен репозиторий для покупателей

---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Application/Common/Interfaces/ICustomersRepository.cs
@@ -0,0 +1,28 @@
+﻿using Marketplace.Users.Domain;
+
+namespace Marketplace.Users.Application.Common.Interfaces;
+
+/// <summary>
+/// Репозиторий покупателей
+/// </summary>
+public interface ICustomersRepository
+{
+    /// <summary>
+    /// Добавить покупателя
+    /// </summary>
+    /// <param name="customer">Покупатель</param>
+    /// <param name="cancellationToken"></param>
+    public Task AddAsync(Customer customer, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Получить покупателя
+    /// </summary>
+    /// <param name="accountId">Идентификатор аккаунта</param>
+    /// <param name="cancellationToken"></param>
+    public Task<Customer> GetAsync(int accountId, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Обновить покупателя
+    /// </summary>
+    /// <param name="customer">Покупатель</param>
+    /// <param name="cancellationToken"></param>
+    public Task UpdateAsync(Customer customer, CancellationToken cancellationToken = default);
+}
---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Infrastructure/Customers/Persistence/CustomersRepository.cs
@@ -0,0 +1,35 @@
+﻿using Marketplace.Users.Application.Common.Exceptions;
+using Marketplace.Users.Application.Common.Interfaces;
+using Marketplace.Users.Domain;
+using Marketplace.Users.Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Users.Infrastructure.Customers.Persistence;
+
+/// <summary>
+/// Репозиторий покупателей
+/// </summary>
+internal class CustomersRepository(MarketplaceContext db) : ICustomersRepository
+{
+    private readonly MarketplaceContext _db = db;
+
+    /// <inheritdoc/>
+    public async Task AddAsync(Customer customer, CancellationToken cancellationToken = default)
+    {
+        await _db.Customers.AddAsync(customer, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task<Customer> GetAsync(int accountId, CancellationToken cancellationToken = default)
+    {
+        var customer = await _db.Customers.FirstOrDefaultAsync(c => c.AccountId == accountId, cancellationToken);
+        return customer is null ? throw new AccountNotExistsException(accountId) : customer;
+    }
+
+    /// <inheritdoc/>
+    public Task UpdateAsync(Customer customer, CancellationToken cancellationToken = default)
+    {
+        _db.Customers.Update(customer);
+        return Task.CompletedTask;
+    }
+}
