350013aebc71d60f3c534086acbfb5949aef123b
Reswero <reswero@mail.ru>
2024-02-07 21:57:41 +0600

    Добавлена команда создания профиля покупателя

---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Application/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Marketplace.Users.Application.Customers.Commands.CreateCustomer;
+
+public record CreateCustomerCommand(int AccountId, string FirstName,
+    string LastName) : IRequest;
---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Application/Customers/Commands/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Marketplace.Users.Application.Common.Interfaces;
+using Marketplace.Users.Domain;
+using MediatR;
+
+namespace Marketplace.Users.Application.Customers.Commands.CreateCustomer;
+
+internal class CreateCustomerCommandHandler(ICustomersRepository repository, IUnitOfWork unitOfWork)
+    : IRequestHandler<CreateCustomerCommand>
+{
+    private readonly ICustomersRepository _repository = repository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        Customer customer = new(request.AccountId, request.FirstName, request.LastName);
+
+        await _repository.AddAsync(customer, cancellationToken);
+        await _unitOfWork.CommitAsync(cancellationToken);
+    }
+}
