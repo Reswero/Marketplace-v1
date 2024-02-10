9be549210d7bcd0371d406e615605d382203c91a
Reswero <reswero@mail.ru>
2024-02-10 21:27:53 +0600

    Сконфигурировано апи (Users)

--- Source/Backend/Csharp/Users/Marketplace.Users.Api/Program.cs
+++ Source/Backend/Csharp/Users/Marketplace.Users.Api/Program.cs
@@ -1,3 +1,5 @@
+using Marketplace.Users.Application;
+using Marketplace.Users.Infrastructure;
 
 namespace Marketplace.Users.Api
 {
@@ -13,6 +15,9 @@ namespace Marketplace.Users.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddApplication();
+            builder.Services.AddInfrastructure(builder.Configuration);
 
             var app = builder.Build();
 
