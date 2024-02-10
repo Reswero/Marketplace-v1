56db259266bd7ca875ed2d00796d3072e3f0450d
Reswero <reswero@mail.ru>
2024-02-11 00:18:45 +0600

    Добавлена конфигурация адресов покупателей для бд (Users)

---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Infrastructure/Addresses/Persistence/AddressConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Marketplace.Users.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Marketplace.Users.Infrastructure.Addresses.Persistence;
+
+internal class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.HasKey(a => new { a.AccountId, a.Location });
+    }
+}
