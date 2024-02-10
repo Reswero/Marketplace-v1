7753f6336791c4f268f183dc25ba0697fa35a2f7
Reswero <reswero@mail.ru>
2024-02-10 21:23:09 +0600

    Добавлены классы с регистрациями зависимостей (Users)

---
+++ Source/Backend/Csharp/Users/Marketplace.Users.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Marketplace.Users.Application.Common.Interfaces;
+using Marketplace.Users.Infrastructure.Common.Persistence;
+using Marketplace.Users.Infrastructure.Customers.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Marketplace.Users.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<MarketplaceContext>(opt =>
+        {
+            opt.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<ICustomersRepository, CustomersRepository>();
+
+        return services;
+    }
+}
