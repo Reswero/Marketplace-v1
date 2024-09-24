cf594ac49d100cf7c594f288d49aefc2c15db2db
Грицутенко Андрей Дмитриевич <critcutenkoad@mir-omsk.ru>
2024-09-24 21:30:34 +0600

    Добавлена проверка доступа к аккаунту (Common-CSharp)

--- Source/Backend/Csharp/Common/Marketplace.Common/Authorization/Extensions/HttpContextExtensions.cs
+++ Source/Backend/Csharp/Common/Marketplace.Common/Authorization/Extensions/HttpContextExtensions.cs
@@ -15,4 +15,22 @@ public static class HttpContextExtensions
     public static AuthorizationClaims GetClaims(this HttpContext context)
         => context.Items.TryGetValue(AuthorizationConsts.ClaimsKey, out var claims) ?
             (AuthorizationClaims) claims : new AuthorizationClaims();
+    
+    /// <summary>
+    /// Проверка доступа к аккаунта по идентификатору
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="id">Идентификатор</param>
+    public static bool CheckAccessById(this HttpContext context, int id)
+    {
+        var claims = context.GetClaims();
+
+        if (claims.Id == id)
+            return true;
+
+        if (claims.Type == AccountType.Admin)
+            return true;
+
+        return false;
+    }
 }
