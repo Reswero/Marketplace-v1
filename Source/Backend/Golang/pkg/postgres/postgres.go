774a4b40526e16c6fc9e0845048bc96c727928ca
Reswero <reswero@mail.ru>
2024-09-26 16:27:07 +0600

    Реализовано автоматическое создание БД (Auth)

--- Source/Backend/Golang/pkg/postgres/postgres.go
+++ Source/Backend/Golang/pkg/postgres/postgres.go
@@ -2,27 +2,39 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Config struct {
+	IpAddress string
+	Port      int
+	User      string
+	Password  string
+	DbName    string
+}
+
 type Storage struct {
 	Pool    *pgxpool.Pool
 	Builder squirrel.StatementBuilderType
 }
 
-func New(connString string) (*Storage, error) {
+func New(c *Config) (*Storage, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
 
-	pool, err := pgxpool.New(ctx, connString)
+	err := createDatabase(ctx, c)
 	if err != nil {
 		return nil, err
 	}
 
-	if err = pool.Ping(ctx); err != nil {
+	pool, err := connectToDb(ctx, c)
+	if err != nil {
 		return nil, err
 	}
 
@@ -36,4 +48,48 @@ func New(connString string) (*Storage, error) {
 
 func (s *Storage) Close() {
 	s.Pool.Close()
+}
+
+func createDatabase(ctx context.Context, c *Config) error {
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%d?sslmode=disable", c.User, c.Password, c.IpAddress, c.Port)
+
+	pool, err := getPool(ctx, connString)
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	var datname string
+	err = pool.QueryRow(ctx, "SELECT datname FROM pg_catalog.pg_database WHERE datname = $1", c.DbName).Scan(&datname)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			_, err = pool.Exec(ctx, fmt.Sprintf("CREATE DATABASE \"%s\"", c.DbName))
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func connectToDb(ctx context.Context, c *Config) (*pgxpool.Pool, error) {
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", c.User, c.Password, c.IpAddress, c.Port, c.DbName)
+	return getPool(ctx, connString)
+}
+
+func getPool(ctx context.Context, connString string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return pool, nil
 }
