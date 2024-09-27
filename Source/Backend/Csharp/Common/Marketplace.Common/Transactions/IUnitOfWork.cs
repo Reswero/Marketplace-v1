3a998bbba422290193a94284d141da4c7b8c1081
Reswero <reswero@mail.ru>
2024-09-27 23:10:45 +0600

    IUnitOfWork вынесен в Common (Common-CSharp)

---
+++ Source/Backend/Csharp/Common/Marketplace.Common/Transactions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Marketplace.Common.Transactions;
+
+/// <summary>
+/// Паттерн Unit Of Work
+/// </summary>
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// Сохранить изменения
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public Task CommitAsync(CancellationToken cancellationToken = default);
+}
