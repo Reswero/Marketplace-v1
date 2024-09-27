0887c26f05108f621277f85e9c8826648fa593a2
Reswero <reswero@mail.ru>
2024-09-27 23:11:04 +0600

    Добавлен контекст БД (Products)

---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Infrastructure/Common/Persistence/ProductsContext.cs
@@ -0,0 +1,30 @@
+﻿using Marketplace.Common.Transactions;
+using Marketplace.Products.Domain.Categories;
+using Marketplace.Products.Domain.Parameters;
+using Marketplace.Products.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Marketplace.Products.Infrastructure.Common.Persistence;
+
+internal class ProductsContext : DbContext, IUnitOfWork
+{
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Discount> Discounts { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Subсategory> Subcategories { get; set; } = null!;
+    public DbSet<CategoryParameter> CategoryParameters { get; set; } = null!;
+    public DbSet<ProductParameter> ProductParameters { get; set; } = null!;
+    public DbSet<Promocode> Promocodes { get; set; } = null!;
+
+    public async Task CommitAsync(CancellationToken cancellationToken = default)
+    {
+        await SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
