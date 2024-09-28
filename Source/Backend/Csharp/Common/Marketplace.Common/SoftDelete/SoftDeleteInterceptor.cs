764524ef1df7ed5e06869e9f614f02c3927c2f66
Reswero <reswero@mail.ru>
2024-09-29 00:42:58 +0600

    Добавлена реализация паттерна Soft Delete

---
+++ Source/Backend/Csharp/Common/Marketplace.Common/SoftDelete/SoftDeleteInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Marketplace.Common.SoftDelete;
+
+/// <summary>
+/// Перехватчик SavingChanges для Soft Delete
+/// </summary>
+public class SoftDeleteInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        MarkObjectsAsDeleted(eventData);
+        return result;
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData,
+        InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        MarkObjectsAsDeleted(eventData);
+        return ValueTask.FromResult(result);
+    }
+
+    private void MarkObjectsAsDeleted(DbContextEventData eventData)
+    {
+        if (eventData.Context is null)
+            return;
+
+        foreach (var entry in eventData.Context.ChangeTracker.Entries())
+        {
+            if (entry.State == EntityState.Deleted && entry is ISoftDelete item)
+            {
+                entry.State = EntityState.Modified;
+                item.SetDeleted();
+            }
+        }
+    }
+}
