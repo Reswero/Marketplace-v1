4e5a44ce9d107776caeedbd796d2a68f9f6db948
Reswero <reswero@mail.ru>
2024-10-06 17:52:03 +0600

    Добавлена авторизация на методы контроллера категорий

--- Source/Backend/Csharp/Products/Marketplace.Products.Api/Controllers/CategoriesController.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Api/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Marketplace.Products.Application.Categories.Commands.CreateCategory;
+﻿using Marketplace.Common.Authorization.Attributes;
+using Marketplace.Common.Authorization.Models;
+using Marketplace.Products.Application.Categories.Commands.CreateCategory;
 using Marketplace.Products.Application.Categories.Commands.DeleteCategory;
 using Marketplace.Products.Application.Categories.Commands.UpdateCategory;
 using Marketplace.Products.Application.Categories.Queries.GetCategory;
@@ -23,7 +25,11 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     /// Создание категории
     /// </summary>
     /// <param name="cmd"></param>
+    [AccountTypeAuthorize(AccountType.Staff)]
     [HttpPost]
+    [ProducesResponseType(typeof(CreateObjectResultVM), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public Task<CreateObjectResultVM> Add(CreateCategoryCommand cmd)
         => _mediator.Send(cmd);
 
@@ -31,7 +37,12 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     /// Получение категории
     /// </summary>
     /// <param name="id">Идентификатор</param>
+    [AccountTypeAuthorize(AccountType.Seller)]
     [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(CategoryVM), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public Task<CategoryVM> Get(int id)
         => _mediator.Send(new GetCategoryQuery(id));
 
@@ -40,7 +51,12 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     /// </summary>
     /// <param name="id">Идентификатор</param>
     /// <param name="cmd"></param>
+    [AccountTypeAuthorize(AccountType.Staff)]
     [HttpPost("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public Task Update(int id, UpdateCategoryCommand cmd)
         => _mediator.Send(cmd);
 
@@ -48,7 +64,12 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     /// Удаление категории
     /// </summary>
     /// <param name="id">Идентификатор</param>
+    [AccountTypeAuthorize(AccountType.Admin)]
     [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public Task<DeleteObjectResultVM> Delete(int id)
         => _mediator.Send(new DeleteCategoryCommand(id));
 }
