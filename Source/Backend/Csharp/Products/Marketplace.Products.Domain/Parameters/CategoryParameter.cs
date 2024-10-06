2e3f14ef52fc83530e6530081d31615feb9c5f12
Reswero <reswero@mail.ru>
2024-10-06 14:07:06 +0600

    Добавлены конструкторы в CategoryParameter

--- Source/Backend/Csharp/Products/Marketplace.Products.Domain/Parameters/CategoryParameter.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Domain/Parameters/CategoryParameter.cs
@@ -1,10 +1,42 @@
-﻿namespace Marketplace.Products.Domain.Parameters;
+﻿using Marketplace.Products.Domain.Categories;
+
+namespace Marketplace.Products.Domain.Parameters;
 
 /// <summary>
 /// Параметр категории
 /// </summary>
 public class CategoryParameter
 {
+    private CategoryParameter() { }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="category">Категория</param>
+    /// <param name="name">Название</param>
+    /// <param name="type">Тип</param>
+    public CategoryParameter(Category category, string name, ParameterType type)
+    {
+        Category = category;
+        Name = name;
+        Type = type;
+    }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="categoryId">Идентификатор категории</param>
+    /// <param name="id">Идентификатор</param>
+    /// <param name="name">Название</param>
+    /// <param name="type">Тип</param>
+    public CategoryParameter(int categoryId, int id, string name, ParameterType type)
+    {
+        CategoryId = categoryId;
+        Id = id;
+        Name = name;
+        Type = type;
+    }
+
     /// <summary>
     /// Идентификатор
     /// </summary>
@@ -13,6 +45,10 @@ public class CategoryParameter
     /// Идентификатор категории
     /// </summary>
     public int CategoryId { get; set; }
+    /// <summary>
+    /// Категория
+    /// </summary>
+    public Category? Category { get; set; } = null;
     /// <summary>
     /// Название
     /// </summary>
