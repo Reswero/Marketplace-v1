5623a2b995b734954096acd5b74221e9980aa5b8
Reswero <reswero@mail.ru>
2024-10-06 17:45:24 +0600

    Изменена проверка типа аккаунта (Common-CSharp)

--- Source/Backend/Csharp/Common/Marketplace.Common/Authorization/Attributes/AccountTypeAuthorizeAttribute.cs
+++ Source/Backend/Csharp/Common/Marketplace.Common/Authorization/Attributes/AccountTypeAuthorizeAttribute.cs
@@ -30,7 +30,7 @@ public class AccountTypeAuthorizeAttribute(AccountType type) : Attribute, IAutho
             return;
         }
 
-        if (claims.Type != _requiredType && claims.Type != AccountType.Admin)
+        if (claims.Type < _requiredType)
         {
             context.Result = new ForbidResult();
             return;
