d9ef328b1e6d838fabbf281f7c08dd4eaa20745f
Reswero <reswero@mail.ru>
2024-10-06 23:44:55 +0600

    Изменен контроллер для категорий

--- Source/Backend/Csharp/Products/Marketplace.Products.Api/Controllers/CategoriesController.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Api/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Marketplace.Common.Authorization.Attributes;
 using Marketplace.Common.Authorization.Models;
+using Marketplace.Common.Responses;
 using Marketplace.Products.Application.Categories.Commands.CreateCategory;
 using Marketplace.Products.Application.Categories.Commands.DeleteCategory;
 using Marketplace.Products.Application.Categories.Commands.UpdateCategory;
@@ -28,12 +29,12 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     [AccountTypeAuthorize(AccountType.Staff)]
     [HttpPost]
     [ProducesResponseType(typeof(CreateObjectResultVM), StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<IActionResult> Add(CreateCategoryCommand cmd)
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> Create(CreateCategoryCommand cmd)
     {
-        await _mediator.Send(cmd);
-        return StatusCode(StatusCodes.Status201Created);
+        var result = await _mediator.Send(cmd);
+        return StatusCode(StatusCodes.Status201Created, result);
     }
 
     /// <summary>
@@ -43,9 +44,9 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     [AccountTypeAuthorize(AccountType.Seller)]
     [HttpGet("{id:int}")]
     [ProducesResponseType(typeof(CategoryVM), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get(int id)
     {
         var category = await _mediator.Send(new GetCategoryQuery(id));
@@ -60,9 +61,9 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     [AccountTypeAuthorize(AccountType.Staff)]
     [HttpPost("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update(int id, UpdateCategoryCommand cmd)
     {
         await _mediator.Send(cmd);
@@ -76,9 +77,9 @@ public class CategoriesController(IMediator mediator) : ControllerBase
     [AccountTypeAuthorize(AccountType.Admin)]
     [HttpDelete("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Delete(int id)
     {
         var result = await _mediator.Send(new DeleteCategoryCommand(id));
