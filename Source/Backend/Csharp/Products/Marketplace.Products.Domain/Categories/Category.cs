8efdadc288da1470355c6248251b856f5226fe37
Reswero <reswero@mail.ru>
2024-10-06 14:05:40 +0600

    Добавлены методы в Category

--- Source/Backend/Csharp/Products/Marketplace.Products.Domain/Categories/Category.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Domain/Categories/Category.cs
@@ -8,26 +8,35 @@ namespace Marketplace.Products.Domain.Categories;
 /// </summary>
 public class Category : ISoftDelete
 {
+    private readonly List<CategoryParameter> _parameters = [];
+
+    private Category() { }
+
+    public Category(string name)
+    {
+        Name = name;
+    }
+
     /// <summary>
     /// Идентификатор
     /// </summary>
-    public int Id { get; set; }
+    public int Id { get; private set; }
     /// <summary>
     /// Название
     /// </summary>
-    public string Name { get; set; } = null!;
+    public string Name { get; private set; } = null!;
     /// <summary>
     /// Параметры
     /// </summary>
-    public List<CategoryParameter>? Parameters { get; set; }
+    public IReadOnlyList<CategoryParameter> Parameters => _parameters;
     /// <summary>
     /// Промокоды
     /// </summary>
-    public List<Promocode>? Promocodes { get; set; }
+    public List<Promocode> Promocodes { get; private set; } = [];
     /// <summary>
     /// Подкатегории
     /// </summary>
-    public List<Subсategory>? Subсategories { get; set; }
+    public List<Subсategory> Subсategories { get; private set; } = [];
     /// <inheritdoc/>
     public DateTimeOffset? DeletedAt { get; private set; }
 
@@ -35,5 +44,36 @@ public class Category : ISoftDelete
     public void SetDeleted()
     {
         DeletedAt = DateTimeOffset.Now;
+    }
+
+    /// <summary>
+    /// Установить название
+    /// </summary>
+    /// <param name="newName">Новое название</param>
+    public void SetName(string newName)
+    {
+        Name = newName;
+    }
+
+    /// <summary>
+    /// Добавить параметры
+    /// </summary>
+    /// <param name="parameters">Параметры</param>
+    public void AddParameters(params CategoryParameter[] parameters)
+    {
+        if (parameters.Length > 0)
+            _parameters.AddRange(parameters);
+    }
+
+    /// <summary>
+    /// Удалить параметры
+    /// </summary>
+    /// <param name="parameters">Параметры</param>
+    public void RemoveParameters(params CategoryParameter[] parameters)
+    {
+        foreach (var parameter in parameters)
+        {
+            _parameters.Remove(parameter);
+        }
     }
 }
