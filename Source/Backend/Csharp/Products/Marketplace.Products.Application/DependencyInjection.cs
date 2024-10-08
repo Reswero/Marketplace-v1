e1184b50aed85bee014b46d85f29ef75839424d6
Reswero <reswero@mail.ru>
2024-10-08 23:05:13 +0600

    Установлена русская локаль для валидации (Products)

--- Source/Backend/Csharp/Products/Marketplace.Products.Application/DependencyInjection.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/DependencyInjection.cs
@@ -2,12 +2,7 @@
 using Marketplace.Common.Mediator.Behaviors;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Marketplace.Products.Application;
 
@@ -22,6 +17,8 @@ public static class DependencyInjection
     /// <param name="services"></param>
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        ValidatorOptions.Global.LanguageManager.Culture = new("ru-RU");
+
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
