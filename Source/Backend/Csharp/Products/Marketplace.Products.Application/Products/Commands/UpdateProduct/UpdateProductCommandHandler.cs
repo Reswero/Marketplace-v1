0915e6f9925dbbd25d4d70af2c4b1bca30139ae8
Reswero <reswero@mail.ru>
2024-10-13 18:03:10 +0600

    Добавлена валидация параметров в команду обновления товара (Products)

--- Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,8 +1,13 @@
-﻿using Marketplace.Common.Transactions;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Marketplace.Common.Transactions;
 using Marketplace.Products.Application.Common.Exceptions;
 using Marketplace.Products.Application.Common.Interfaces;
+using Marketplace.Products.Application.Products.ViewModels;
+using Marketplace.Products.Application.Products.ViewModels.Validators;
 using Marketplace.Products.Domain.Categories;
 using Marketplace.Products.Domain.Parameters;
+using Marketplace.Products.Domain.Products;
 using MediatR;
 
 namespace Marketplace.Products.Application.Products.Commands.UpdateProduct;
@@ -26,22 +31,46 @@ internal class UpdateProductCommandHandler(IProductsRepository productsRepositor
 
         product.ChangeSubcategory(subcategory);
 
-        var categoryParametersIds = category.Parameters.Select(p => p.Id).ToHashSet();
-        var parametersIds = product.Parameters.Select(p => p.Id).ToHashSet();
-
-        var unexistingParameters = request.Parameters.Where(p => p.Id == 0 &&
-            categoryParametersIds.Contains(p.CategoryParameterId));
-        var existingParameters = request.Parameters.Where(p => parametersIds.Contains(p.Id) &&
-            categoryParametersIds.Contains(p.CategoryParameterId));
-
-        var parameters = existingParameters.Union(unexistingParameters)
-            .Select(p => new ProductParameter(p.Id, p.CategoryParameterId, product.Id, p.Value))
-            .ToArray();
+        var parameters = await GetParametersAsync(category, product, request.Parameters, cancellationToken);
 
         product.RemoveParameters([.. product.Parameters]);
         product.AddParameters(parameters);
 
         await _productsRepository.UpdateAsync(product, cancellationToken);
         await _unitOfWork.CommitAsync(cancellationToken);
+    }
+
+    private static async Task<ProductParameter[]> GetParametersAsync(Category category, Product product,
+        List<UpdateProductParameterVM> requestParameters, CancellationToken cancellationToken)
+    {
+        var categoryParameters = category.Parameters.ToDictionary(p => p.Id);
+        var categoryParametersIds = categoryParameters.Keys.ToHashSet();
+        var parametersIds = product.Parameters.Select(p => p.Id).ToHashSet();
+
+        var unexistingParameters = requestParameters.Where(p => p.Id == 0 &&
+            categoryParametersIds.Contains(p.CategoryParameterId));
+        var existingParameters = requestParameters.Where(p => parametersIds.Contains(p.Id) &&
+            categoryParametersIds.Contains(p.CategoryParameterId));
+
+        var parametersVMs = existingParameters.Union(unexistingParameters);
+
+        List<ValidationFailure> validationErrors = [];
+        foreach (var vm in parametersVMs)
+        {
+            var categoryParameter = categoryParameters[vm.CategoryParameterId];
+
+            UpdateProductParameterVMValidator validator = new(categoryParameter);
+            var result = await validator.ValidateAsync(vm, cancellationToken);
+
+            validationErrors.AddRange(result.Errors);
+        }
+
+        if (validationErrors.Count > 0)
+        {
+            throw new ValidationException(validationErrors);
+        }
+
+        return parametersVMs.Select(p => new ProductParameter(p.Id, p.CategoryParameterId, product.Id, p.Value))
+            .ToArray();
     }
 }
