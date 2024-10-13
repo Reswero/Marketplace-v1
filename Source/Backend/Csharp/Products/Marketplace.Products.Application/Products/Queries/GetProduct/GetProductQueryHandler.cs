269abc3c89aafe1227cf499ee6751801d483a9db
Reswero <reswero@mail.ru>
2024-10-13 15:20:38 +0600

    Добавлен статус для модели товара (Products)

--- Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/Queries/GetProduct/GetProductQueryHandler.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/Queries/GetProduct/GetProductQueryHandler.cs
@@ -27,7 +27,8 @@ internal class GetProductQueryHandler(IProductsRepository repository)
             Subcategory = new(product.Subcategory!.Id, product.Subcategory!.Name),
             Discount = discount is not null ? new(discount.Size, discount.ValidUntil) : null,
             Parameters = product.Parameters.Select(p =>
-                new ProductParameterVM(p.CategoryParameter!.Name, p.Value)).ToList()
+                new ProductParameterVM(p.CategoryParameter!.Name, p.Value)).ToList(),
+            Status = product.DeletedAt is null ? ProductStatus.Available : ProductStatus.Deleted
         };
     }
 }
--- Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/ViewModels/ProductVM.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/ViewModels/ProductVM.cs
@@ -2,15 +2,49 @@
 
 namespace Marketplace.Products.Application.Products.ViewModels;
 
-internal class ProductVM
+/// <summary>
+/// Модель товара
+/// </summary>
+public class ProductVM
 {
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
     public int Id { get; set; }
+    /// <summary>
+    /// Продавец
+    /// </summary>
     public SellerVM Seller { get; set; } = null!;
+    /// <summary>
+    /// Категория
+    /// </summary>
     public ShortCategoryVM Category { get; set; } = null!;
+    /// <summary>
+    /// Подкатегория
+    /// </summary>
     public SubcategoryVM Subcategory { get; set; } = null!;
+    /// <summary>
+    /// Название
+    /// </summary>
     public string Name { get; set; } = null!;
+    /// <summary>
+    /// Описание
+    /// </summary>
     public string Description { get; set; } = null!;
+    /// <summary>
+    /// Цена
+    /// </summary>
     public int Price { get; set; }
+    /// <summary>
+    /// Скидка
+    /// </summary>
     public DiscountVM? Discount { get; set; }
+    /// <summary>
+    /// Параметры
+    /// </summary>
     public List<ProductParameterVM> Parameters { get; set; } = null!;
+    /// <summary>
+    /// Статус
+    /// </summary>
+    public ProductStatus Status { get; set; }
 }
