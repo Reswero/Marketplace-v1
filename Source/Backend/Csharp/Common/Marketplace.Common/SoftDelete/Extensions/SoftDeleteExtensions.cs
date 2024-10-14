d4632caed22e95995be26eb6501438da5bb79420
Reswero <reswero@mail.ru>
2024-10-14 23:12:11 +0600

    Добавлен метод расширения для ISoftDelete (Common-CSharp)

---
+++ Source/Backend/Csharp/Common/Marketplace.Common/SoftDelete/Extensions/SoftDeleteExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Marketplace.Common.SoftDelete.Exceptions;
+
+namespace Marketplace.Common.SoftDelete.Extensions;
+
+/// <summary>
+/// Расширения для интерфейса <see cref="ISoftDelete"/>
+/// </summary>
+public static class SoftDeleteExtensions
+{
+    /// <summary>
+    /// Выбросить исключение, если объект удален
+    /// </summary>
+    /// <param name="softDelete"></param>
+    /// <exception cref="ObjectDeletedException"></exception>
+    public static void ThrowIfDeleted(this ISoftDelete softDelete)
+    {
+        if (softDelete.DeletedAt is null)
+            return;
+
+        throw new ObjectDeletedException();
+    }
+}
