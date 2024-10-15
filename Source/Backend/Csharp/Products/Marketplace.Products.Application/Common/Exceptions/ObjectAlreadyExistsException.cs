225a830db89f5b3314b5268d252e9701a4984fbc
Reswero <reswero@mail.ru>
2024-10-15 19:24:07 +0600

    Добавлен ObjectAlreadyExistsException (Products)

---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Common/Exceptions/ObjectAlreadyExistsException.cs
@@ -0,0 +1,10 @@
+﻿namespace Marketplace.Products.Application.Common.Exceptions;
+
+/// <summary>
+/// Объект уже существует
+/// </summary>
+/// <param name="type">Тип</param>
+/// <param name="name">Название</param>
+public class ObjectAlreadyExistsException(Type type, string name)
+    : Exception($"Объект типа \"{type.Name}\" с названием \"{name}\" уже существует")
+{ }
