faa5024d2318e1f321b4b6b3e7270fefe7f42871
Reswero <reswero@mail.ru>
2024-10-19 20:16:43 +0600

    Добавлен сервис UsersService (Products)

---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Users/Models/Seller.cs
@@ -0,0 +1,8 @@
+﻿namespace Marketplace.Products.Application.Users.Models;
+
+/// <summary>
+/// Продавец
+/// </summary>
+/// <param name="AccountId">Идентификатор аккаунта</param>
+/// <param name="CompanyName">Название компании</param>
+public record Seller(int AccountId, string CompanyName);
---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Infrastructure/Users/Services/UsersService.cs
@@ -0,0 +1,33 @@
+﻿using Marketplace.Products.Application.Common.Exceptions;
+using Marketplace.Products.Application.Common.Interfaces;
+using Marketplace.Products.Application.Users.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace Marketplace.Products.Infrastructure.Users.Services;
+
+/// <summary>
+/// Взаимодействие с сервисом пользователей
+/// </summary>
+internal class UsersService(HttpClient httpClient)
+    : IUsersService
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    /// <inheritdoc/>
+    public async Task<Seller> GetSellerInfoAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var response = await _httpClient.GetAsync($"/v1/sellers/{id}/info", cancellationToken);
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            throw new ObjectNotFoundException(typeof(Seller), id);
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+        var seller = JsonSerializer.Deserialize<Seller>(stream)!;
+
+        return seller;
+    }
+}
