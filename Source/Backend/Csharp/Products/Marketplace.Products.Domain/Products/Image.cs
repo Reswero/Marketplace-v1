d9f72ba96d65c726a6a6258efc178634800f1ac5
Reswero <reswero@mail.ru>
2024-10-21 00:12:09 +0600

    Добавлена сущность Image (Products)

---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Domain/Products/Image.cs
@@ -0,0 +1,45 @@
+﻿namespace Marketplace.Products.Domain.Products;
+
+/// <summary>
+/// Изображение товара
+/// </summary>
+public class Image
+{
+    private Image() { }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="productId">Идентификатор товара</param>
+    /// <param name="order">Порядок</param>
+    /// <param name="bucketName">Название bucket'а</param>
+    /// <param name="name">Название изображения</param>
+    public Image(int productId, int order, string bucketName, string name)
+    {
+        ProductId = productId;
+        Order = order;
+        BucketName = bucketName;
+        Name = name;
+    }
+
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Идентификатор товара
+    /// </summary>
+    public int ProductId { get; set; }
+    /// <summary>
+    /// Порядок
+    /// </summary>
+    public int Order { get; set; }
+    /// <summary>
+    /// Название bucket'а
+    /// </summary>
+    public string BucketName { get; set; } = null!;
+    /// <summary>
+    /// Название изображения
+    /// </summary>
+    public string Name { get; set; } = null!;
+}
