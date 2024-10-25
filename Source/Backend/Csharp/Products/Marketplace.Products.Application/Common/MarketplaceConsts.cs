05dca7689ff46ec5f2a0759200651c99a68c563f
Reswero <reswero@mail.ru>
2024-10-26 02:04:46 +0600

    Добавлена валидация на команду загрузки изображений (Products)

--- Source/Backend/Csharp/Products/Marketplace.Products.Application/Common/MarketplaceConsts.cs
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Common/MarketplaceConsts.cs
@@ -5,5 +5,12 @@
 /// </summary>
 public static class MarketplaceConsts
 {
+    /// <summary>
+    /// Максимальный размер изображения в байтах
+    /// </summary>
+    public const int MaxImageSize = 5_242_880;
+    /// <summary>
+    /// Бакет с изображениями товаров
+    /// </summary>
     public const string ProductsImagesBucket = "products-bucket";
 }
---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/Commands/UploadImages/UploadImagesCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Marketplace.Products.Application.Common;
+
+namespace Marketplace.Products.Application.Products.Commands.UploadImages;
+
+internal class UploadImagesCommandValidator : AbstractValidator<UploadImagesCommand>
+{
+    public UploadImagesCommandValidator()
+    {
+        RuleFor(c => c.Images).NotEmpty();
+        RuleFor(c => c.Images).ForEach(file =>
+        {
+            file.ChildRules(validator =>
+            {
+                validator.RuleFor(f => f.Stream.Length).LessThanOrEqualTo(MarketplaceConsts.MaxImageSize);
+            });
+        });
+    }
+}
