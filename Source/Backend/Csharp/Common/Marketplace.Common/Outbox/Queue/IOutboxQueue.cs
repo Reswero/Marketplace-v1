92da5bb67df9fd4e92d9d7192223bdefead3832d
Reswero <reswero@mail.ru>
2024-10-27 23:10:33 +0600

    Добавлена реализация паттерна Outbox на основе очереди (Common-CSharp)

---
+++ Source/Backend/Csharp/Common/Marketplace.Common/Outbox/Queue/IOutboxQueue.cs
@@ -0,0 +1,44 @@
+﻿namespace Marketplace.Common.Outbox.Queue;
+
+/// <summary>
+/// Паттерн Outbox на основе очереди
+/// </summary>
+/// <typeparam name="T">Тип элемента</typeparam>
+public interface IOutboxQueue<T>
+{
+    /// <summary>
+    /// Добавить элемент в конец очереди
+    /// </summary>
+    /// <param name="item">Элемент</param>
+    /// <param name="cancellationToken"></param>
+    public Task PushAsync(T item, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Добавить элементы в конец очереди
+    /// </summary>
+    /// <param name="items">Элементы</param>
+    /// <param name="cancellationToken"></param>
+    public Task PushAsync(T[] items, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Получить первый элемент из очереди
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public Task<T> PeekAsync(CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Получить несколько первых элементов из очереди
+    /// </summary>
+    /// <param name="count">Количество</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<List<T>> PeekAsync(int count, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Получить и удалить первый элемент из очереди
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public Task<T> PopAsync(CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Получить и удалить несколько первыз элментов из очереди
+    /// </summary>
+    /// <param name="count">Количество</param>
+    /// <param name="cancellationToken"></param>
+    public Task<List<T>> PopAsync(int count, CancellationToken cancellationToken = default);
+}
---
+++ Source/Backend/Csharp/Common/Marketplace.Common/Outbox/Queue/OutboxQueue.cs
@@ -0,0 +1,131 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System.Text.Json;
+
+namespace Marketplace.Common.Outbox.Queue;
+
+/// <summary>
+/// Реализация паттерна Outbox на основе очереди
+/// </summary>
+/// <typeparam name="T">Тип элемента</typeparam>
+public class OutboxQueue<T> : IOutboxQueue<T>
+{   
+    private const string _tableName = "queue";
+    private const string _sqlCreateTable = $"""
+        CREATE TABLE IF NOT EXISTS {_tableName} (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            value TEXT NOT NULL
+        );
+        """;
+    private const string _sqlInsert = $"""
+        INSERT INTO {_tableName} (value)
+        VALUES (@value);
+        """;
+    private const string _sqlSelectValue = $"""
+        SELECT value FROM {_tableName}
+        ORDER BY id
+        LIMIT @count;
+        """;
+    private const string _sqlSelectAll = $"""
+        SELECT * FROM {_tableName}
+        ORDER BY id
+        LIMIT @count;
+        """;
+    private const string _sqlDelete = $"""
+        DELETE FROM {_tableName}
+        WHERE id in @ids;
+        """;
+
+    private readonly string _connectionString;
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="databaseName">Название базы данных</param>
+    /// <param name="pooling">Включен ли пулинг соединений</param>
+    public OutboxQueue(string databaseName, bool pooling = true)
+    {
+        SqliteConnectionStringBuilder builder = new()
+        {
+            DataSource = databaseName,
+            Pooling = pooling
+        };
+
+        _connectionString = builder.ToString();
+        CreateTable();
+    }
+
+    /// <inheritdoc/>
+    public async Task PushAsync(T item, CancellationToken cancellationToken = default)
+        => await PushAsync([item], cancellationToken);
+
+    /// <inheritdoc/>
+    public async Task PushAsync(T[] items, CancellationToken cancellationToken = default)
+    {
+        using var connection = CreateConnection();
+
+        List<string> values = new(items.Length);
+        foreach (var item in items)
+        {
+            var value = JsonSerializer.Serialize(item);
+            values.Add(value);
+        }
+
+        await connection.OpenAsync(cancellationToken);
+        var tx = await connection.BeginTransactionAsync(cancellationToken);
+        await connection.ExecuteAsync(_sqlInsert, values.Select(v => new { value = v }));
+        await tx.CommitAsync(cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task<T> PeekAsync(CancellationToken cancellationToken = default)
+        => (await PeekAsync(1, cancellationToken)).Single();
+
+    /// <inheritdoc/>
+    public async Task<List<T>> PeekAsync(int count, CancellationToken cancellationToken = default)
+    {
+        using var connection = CreateConnection();
+        var values = await connection.QueryAsync<string>(_sqlSelectValue, new { count });
+
+        List<T> items = new(count);
+        foreach (var value in values)
+        {
+            var item = JsonSerializer.Deserialize<T>(value)!;
+            items.Add(item);
+        }
+
+        return items;
+    }
+
+    /// <inheritdoc/>
+    public async Task<T> PopAsync(CancellationToken cancellationToken = default)
+        => (await PopAsync(1, cancellationToken)).Single();
+
+    /// <inheritdoc/>
+    public async Task<List<T>> PopAsync(int count, CancellationToken cancellationToken = default)
+    {
+        using var connection = CreateConnection();
+        var rows = await connection.QueryAsync(_sqlSelectAll, new { count });
+
+        List<T> items = new(count);
+        foreach (var row in rows)
+        {
+            T item = JsonSerializer.Deserialize<T>(row.value)!;
+            items.Add(item);
+        }
+
+        var ids = rows.Select(r => r.id).ToList();
+        await connection.ExecuteAsync(_sqlDelete, new { ids });
+
+        return items;
+    }
+
+    private SqliteConnection CreateConnection()
+        => new(_connectionString);
+
+    private void CreateTable()
+    {
+        using var connection = CreateConnection();
+        connection.Execute(_sqlCreateTable);
+    }
+}
