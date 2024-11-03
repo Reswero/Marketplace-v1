5b4c458ec36f38f157a8df3d7370b4273d0b0216
Reswero <reswero@mail.ru>
2024-11-03 21:00:37 +0600

    Добавлен запрос на получение полной информации о товаре

---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/ViewModels/ImageVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Marketplace.Products.Application.Products.ViewModels;
+
+/// <summary>
+/// Модель изображения
+/// </summary>
+/// <param name="Id">Идентификатор</param>
+/// <param name="Order">Порядок</param>
+/// <param name="Name">Название</param>
+public record ImageVM(int Id, int Order, string Name);
---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/ViewModels/ProductFullInfoVM.cs
@@ -0,0 +1,62 @@
+﻿using Marketplace.Products.Application.Categories.ViewModels;
+
+namespace Marketplace.Products.Application.Products.ViewModels;
+
+/// <summary>
+/// Модель товара
+/// </summary>
+public record ProductFullInfoVM
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Категория
+    /// </summary>
+    public ShortCategoryVM Category { get; set; } = null!;
+    /// <summary>
+    /// Подкатегория
+    /// </summary>
+    public SubcategoryVM Subcategory { get; set; } = null!;
+    /// <summary>
+    /// Название
+    /// </summary>
+    public string Name { get; set; } = null!;
+    /// <summary>
+    /// Описание
+    /// </summary>
+    public string Description { get; set; } = null!;
+    /// <summary>
+    /// Цена
+    /// </summary>
+    public int Price { get; set; }
+    /// <summary>
+    /// Скидки
+    /// </summary>
+    public List<DiscountVM>? Discounts { get; set; }
+    /// <summary>
+    /// Параметры
+    /// </summary>
+    public List<ProductParameterVM> Parameters { get; set; } = null!;
+    /// <summary>
+    /// Изображения
+    /// </summary>
+    public List<ImageVM> Images { get; set; } = null!;
+    /// <summary>
+    /// Статус
+    /// </summary>
+    public ProductStatus Status { get; set; }
+    /// <summary>
+    /// Дата создания
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; set; }
+    /// <summary>
+    /// Дата обновления
+    /// </summary>
+    public DateTimeOffset? UpdatedAt { get; set; }
+    /// <summary>
+    /// Дата удаления
+    /// </summary>
+    public DateTimeOffset? DeletedAt { get; set; }
+}
