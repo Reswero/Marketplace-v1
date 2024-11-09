79672aaae1860d296bf6c865bbb7115ac0afef20
Reswero <reswero@mail.ru>
2024-11-10 01:01:24 +0600

    Добавлен http сервер (Favorites)

---
+++ Source/Backend/Golang/favorites/internal/delivery/http/delivery.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/Reswero/Marketplace-v1/favorites/internal/usecase"
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// АПИ
+type Delivery struct {
+	router      *echo.Echo
+	logger      *slog.Logger
+	ucFavorites usecase.Favorites
+}
+
+func New(logger *slog.Logger, env string, ucFavorites usecase.Favorites) *Delivery {
+	d := &Delivery{
+		logger:      logger,
+		ucFavorites: ucFavorites,
+	}
+
+	d.router = echo.New()
+
+	if env == "dev" {
+		d.router.GET("/swagger/*", echoSwagger.WrapHandler)
+	}
+
+	return d
+}
+
+// Запустить http сервер
+func (d *Delivery) Start(address string) error {
+	return d.router.Start(address)
+}
+
+// Остановить http сервер
+func (d *Delivery) Close() error {
+	return d.router.Close()
+}
+
+// Залоггировать ошибку
+func (d *Delivery) logError(op, message string, err error) {
+	err = fmt.Errorf("%s: %w", op, err)
+	d.logger.Error(message, slog.String("error", err.Error()))
+}
