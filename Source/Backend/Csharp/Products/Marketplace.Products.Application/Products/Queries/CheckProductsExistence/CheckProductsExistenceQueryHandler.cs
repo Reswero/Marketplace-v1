7069b600146cf141c70620e0086c6fa4420e1e40
Reswero <reswero@mail.ru>
2025-01-18 23:55:56 +0600

    Добавлен метод на проверку существования товаров (Products)

---
+++ Source/Backend/Csharp/Products/Marketplace.Products.Application/Products/Queries/CheckProductsExistence/CheckProductsExistenceQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Marketplace.Products.Application.Common.Interfaces;
+using MediatR;
+
+namespace Marketplace.Products.Application.Products.Queries.CheckProductsExistence;
+
+internal class CheckProductsExistenceQueryHandler(IProductsRepository repository)
+    : IRequestHandler<CheckProductsExistenceQuery, List<int>>
+{
+    private readonly IProductsRepository _repository = repository;
+
+    public async Task<List<int>> Handle(CheckProductsExistenceQuery request, CancellationToken cancellationToken)
+    {
+        var products = await _repository.GetAsync(request.Ids, cancellationToken);
+        return products.Select(p => p.Id).ToList();
+    }
+}
