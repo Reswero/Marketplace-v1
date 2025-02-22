118e2c3585661d03f2c161b98d688a9dd6063547
Reswero <reswero@mail.ru>
2025-02-22 22:55:19 +0600

    Добавлен DI для инфраструктурного слоя

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using Marketplace.Orders.Application.Common.Interfaces;
+using Marketplace.Orders.Infrastructure.Common.Persistence;
+using Marketplace.Orders.Infrastructure.Orders.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Marketplace.Orders.Infrastructure;
+
+/// <summary>
+/// Инъекция зависимостей слоя инфраструктуры
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Добавить слой инфраструктуры
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration">Конфигурация</param>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabase(configuration);
+
+        services.AddScoped<IOrdersRepository, OrdersRepository>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Инициализировать базу данных
+    /// </summary>
+    /// <param name="provider"></param>
+    public static void InitializeDatabase(this IServiceProvider provider)
+    {
+        using var scope = provider.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<OrdersContext>();
+        
+        db.Database.EnsureCreated();
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextFactory<OrdersContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            options.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+}
