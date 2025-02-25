088e82e2a59f5b8d7a901c2d515bff3b56ee9d49
Reswero <reswero@mail.ru>
2025-02-25 21:04:11 +0600

    Добавлена команда создания заказа (Orders)

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Application/Common/ViewModels/CreateObjectResultVM.cs
@@ -0,0 +1,7 @@
+﻿namespace Marketplace.Orders.Application.Common.ViewModels;
+
+/// <summary>
+/// Модель результата создания объекта
+/// </summary>
+/// <param name="Id">Идентификатор объекта</param>
+public record CreateObjectResultVM(long Id);
---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using Marketplace.Common.Transactions;
+using Marketplace.Orders.Application.Common.Exceptions;
+using Marketplace.Orders.Application.Common.Interfaces;
+using Marketplace.Orders.Application.Common.ViewModels;
+using Marketplace.Orders.Domain;
+using MediatR;
+
+namespace Marketplace.Orders.Application.Orders.Commands.CreateOrder;
+
+internal class CreateOrderCommandHandler(IOrdersRepository repository, IUnitOfWork unitOfWork,
+    IProductsServiceClient productsClient)
+    : IRequestHandler<CreateOrderCommand, CreateObjectResultVM>
+{
+    private readonly IOrdersRepository _repository = repository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IProductsServiceClient _productsClient = productsClient;
+
+    public async Task<CreateObjectResultVM> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        Order order = new(request.CustomerId);
+
+        var requestProducts = request.Products.ToDictionary(p => p.Id);
+        var products = await _productsClient.GetProductsByIdsAsync([.. requestProducts.Keys], cancellationToken);
+        var productsDictionary = products.ToDictionary(p => p.Id);
+
+        foreach (var requestProduct in requestProducts)
+        {
+            if (productsDictionary.ContainsKey(requestProduct.Key) is false)
+                throw new ProductNotFoundException(requestProduct.Key);
+        }
+
+        var orderProducts = new OrderProduct[products.Count];
+        for (int i = 0; i < products.Count; i++)
+        {
+            var product = products[i];
+            var requestProduct = requestProducts[product.Id];
+
+            OrderProduct orderProduct = new(order, product.Id, product.SellerId,
+                requestProduct.Quantity, product.Price, product.DiscountSize);
+            orderProducts[i] = orderProduct;
+        }
+
+        order.AddProducts(orderProducts);
+
+        OrderStatus createdStatus = new(order, OrderStatusType.Created);
+        order.AddStatus(createdStatus);
+
+        await _repository.AddAsync(order, cancellationToken);
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        // TODO: call to payment service
+        OrderStatus pendingStatus = new(order, OrderStatusType.Pending);
+        order.AddStatus(pendingStatus);
+
+        await _repository.UpdateAsync(order, cancellationToken);
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        // TODO: return link to payment
+        return new CreateObjectResultVM(order.Id);
+    }
+}
