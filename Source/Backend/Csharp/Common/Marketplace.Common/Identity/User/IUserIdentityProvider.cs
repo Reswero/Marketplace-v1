db02cb7f3a890e27524a3ee6f2763419cf650a53
Reswero <reswero@mail.ru>
2025-02-25 21:27:40 +0600

    Добавлен пакет для идентификационных данных (Common)

---
+++ Source/Backend/Csharp/Common/Marketplace.Common/Identity/User/IUserIdentityProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Marketplace.Common.Identity.User;
+
+/// <summary>
+/// Провайдер идентификационных данных пользователя
+/// </summary>
+public interface IUserIdentityProvider
+{
+    /// <summary>
+    /// Идентификатор пользователя
+    /// </summary>
+    public int? Id { get; }
+}
---
+++ Source/Backend/Csharp/Common/Marketplace.Common/Identity/User/UserIdentityProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Marketplace.Common.Identity.User;
+
+/// <summary>
+/// Расширения для провайдера идентификационных данных пользователя
+/// </summary>
+public static class UserIdentityProviderExtensions
+{
+    /// <summary>
+    /// Добавить провайдер идентификационных данных пользователя из HttpContext'а
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddHttpUserIdentityProvider(this IServiceCollection services)
+        => services.AddScoped<IUserIdentityProvider, HttpUserIdentityProvider>();
+}
