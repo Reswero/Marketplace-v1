3d7353d84733dba476b5965ec3ec7384c9ea5be9
Reswero <reswero@mail.ru>
2025-03-01 00:02:01 +0600

    Добавлена сущность NewOrder (Orders)

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Domain/NewOrder.cs
@@ -0,0 +1,27 @@
+﻿namespace Marketplace.Orders.Domain;
+
+/// <summary>
+/// Новый заказ
+/// </summary>
+public class NewOrder
+{
+    private NewOrder() { }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="order">Заказ</param>
+    public NewOrder(Order order)
+    {
+        Id = order.Id;
+    }
+
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public long Id { get; private set; }
+    /// <summary>
+    /// Заказ
+    /// </summary>
+    public Order? Order { get; private set; }
+}
