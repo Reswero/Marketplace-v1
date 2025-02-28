7d4b61614955db2ed8254d4ea8d658326128e326
Reswero <reswero@mail.ru>
2025-02-28 14:51:53 +0600

    Добавлен запрос на получение заказа покупателя (Orders)

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Application/Orders/Queries/GetCustomerOrder/GetCustomerOrderQuery.cs
@@ -0,0 +1,10 @@
+﻿using Marketplace.Orders.Application.Orders.ViewModels;
+using MediatR;
+
+namespace Marketplace.Orders.Application.Orders.Queries.GetCustomerOrder;
+
+/// <summary>
+/// Запрос на получение заказа покупателя
+/// </summary>
+/// <param name="OrderId">Идентификатор товара</param>
+public record GetCustomerOrderQuery(long OrderId) : IRequest<OrderVM>;
