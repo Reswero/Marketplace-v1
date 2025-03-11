c1306e99c14427c91ce91a7e2d049880d04c54a6
Reswero <reswero@mail.ru>
2025-03-11 22:06:11 +0600

    Реализованы методы для OrderProductsRepository из интерфейса (Orders)

--- Source/Backend/Csharp/Orders/Marketplace.Orders.Infrastructure/Orders/Persistence/OrderProductsRepository.cs
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Infrastructure/Orders/Persistence/OrderProductsRepository.cs
@@ -1,4 +1,5 @@
-﻿using Marketplace.Orders.Application.Common.Interfaces;
+﻿using Marketplace.Orders.Application.Common.Exceptions;
+using Marketplace.Orders.Application.Common.Interfaces;
 using Marketplace.Orders.Application.Common.Models;
 using Marketplace.Orders.Domain;
 using Marketplace.Orders.Infrastructure.Common.Persistence;
@@ -16,15 +17,32 @@ internal class OrderProductsRepository(OrdersContext db)
     private readonly OrdersContext _db = db;
 
     /// <inheritdoc/>
+    public async Task<OrderProduct> GetAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await _db.OrderProducts.Include(p => p.Statuses)
+            .Where(p => p.Id == id)
+            .FirstOrDefaultAsync(cancellationToken)
+                ?? throw new ProductNotFoundException(id);
+    }
+
+    /// <inheritdoc/>
     public async Task<List<OrderProduct>> GetBySellerAsync(int sellerId, Pagination pagination,
         CancellationToken cancellationToken = default)
     {
-        return await _db.OrderProducts.Include(p => p.Order)
-            .Include(p => p.Statuses)
+        return await _db.OrderProducts.Include(p => p.Statuses)
+            .Include(p => p.Order)
+                .ThenInclude(o => o!.Statuses)
             .Where(p => p.SellerId == sellerId)
             .OrderByDescending(p => p.Order!.CreatedAt)
             .Skip(pagination.Offset)
             .Take(pagination.Limit)
             .ToListAsync(cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public Task UpdateAsync(OrderProduct product, CancellationToken cancellationToken = default)
+    {
+        _db.OrderProducts.Update(product);
+        return Task.CompletedTask;
     }
 }
