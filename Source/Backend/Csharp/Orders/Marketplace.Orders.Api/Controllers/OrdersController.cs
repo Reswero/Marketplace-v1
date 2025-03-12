2e7a71c01e3414264513dc0b43ceb68c36377c53
Reswero <reswero@mail.ru>
2025-03-12 21:57:35 +0600

    Добавлены атрибуты к методам контроллера для swagger'а (Orders)

--- Source/Backend/Csharp/Orders/Marketplace.Orders.Api/Controllers/OrdersController.cs
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Api/Controllers/OrdersController.cs
@@ -1,5 +1,7 @@
 ﻿using Marketplace.Common.Authorization.Attributes;
 using Marketplace.Common.Authorization.Models;
+using Marketplace.Common.Responses;
+using Marketplace.Orders.Application.Common.ViewModels;
 using Marketplace.Orders.Application.Orders.Commands.CancelOrder;
 using Marketplace.Orders.Application.Orders.Commands.CreateOrder;
 using Marketplace.Orders.Application.Orders.Commands.PackOrderProduct;
@@ -7,6 +9,7 @@ using Marketplace.Orders.Application.Orders.Queries.GetCustomerOrder;
 using Marketplace.Orders.Application.Orders.Queries.GetCustomerOrders;
 using Marketplace.Orders.Application.Orders.Queries.GetOrderPaymentLink;
 using Marketplace.Orders.Application.Orders.Queries.GetSellerOrders;
+using Marketplace.Orders.Application.Orders.ViewModels;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +29,10 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// </summary>
     /// <param name="cmd"></param>
     [HttpPost("/internal/v1/orders")]
+    [ProducesResponseType(typeof(CreateObjectResultVM), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Create(CreateOrderCommand cmd)
     {
         var result = await _mediator.Send(cmd);
@@ -38,6 +45,12 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// <param name="query"></param>
     [HttpGet("customer/orders")]
     [AccountTypeAuthorize(AccountType.Customer)]
+    [ProducesResponseType(typeof(List<OrderShortInfoVM>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetCustomerOrders([FromQuery] GetCustomerOrdersQuery query)
     {
         var orders = await _mediator.Send(query);
@@ -50,6 +63,11 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// <param name="orderId">Идентификатор заказа</param>
     [HttpGet("customer/orders/{orderId:long}")]
     [AccountTypeAuthorize(AccountType.Customer)]
+    [ProducesResponseType(typeof(OrderVM), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetCustomerOrder(long orderId)
     {
         var order = await _mediator.Send(new GetCustomerOrderQuery(orderId));
@@ -62,6 +80,12 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// <param name="orderId">Идентификатор заказа</param>
     [HttpGet("customer/orders/{orderId:long}/payment")]
     [AccountTypeAuthorize(AccountType.Customer)]
+    [ProducesResponseType(typeof(PaymentVM), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetOrderPaymentLink(long orderId)
     {
         var payment = await _mediator.Send(new GetOrderPaymentLinkQuery(orderId));
@@ -75,6 +99,12 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// <param name="productIds">Идентификаторы товаров</param>
     [HttpDelete("customer/orders/{orderId:long}")]
     [AccountTypeAuthorize(AccountType.Customer)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> CancelOrder(long orderId, [FromQuery] int[]? productIds)
     {
         await _mediator.Send(new CancelOrderCommand(orderId, productIds));
@@ -87,6 +117,11 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// <param name="query"></param>
     [HttpGet("seller/orders")]
     [AccountTypeAuthorize(AccountType.Seller)]
+    [ProducesResponseType(typeof(List<SellerOrderVM>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetSellerOrders([FromQuery] GetSellerOrdersQuery query)
     {
         var orders = await _mediator.Send(query);
@@ -99,6 +134,11 @@ public class OrdersController(IMediator mediator) : ControllerBase
     /// <param name="orderProductId">Идентификатор товара из заказа</param>
     [HttpPost("seller/orders/{orderProductId:int}/pack")]
     [AccountTypeAuthorize(AccountType.Seller)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> PackOrder(int orderProductId)
     {
         await _mediator.Send(new PackOrderProductCommand(orderProductId));
