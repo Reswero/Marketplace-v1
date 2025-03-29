4e75135d9e3aaac0d4bb862fd93abd5a297f3828
Reswero <reswero@mail.ru>
2025-03-29 23:34:04 +0600

    Добавлен конфиг (Payment)

---
+++ Source/Backend/Golang/payment/internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Grpc `yaml:"grpc_server"`
+}
+
+type Grpc struct {
+	Address string `yaml:"address"`
+}
+
+func MustLoad() *Config {
+	path := os.Getenv("CONFIG_PATH")
+	if path == "" {
+		panic("CONFIG_PATH is not setted")
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		panic(err)
+	}
+
+	cfg := &Config{}
+	if err := cleanenv.ReadConfig(path, *cfg); err != nil {
+		panic(fmt.Sprintf("error while reading config. %w", err))
+	}
+
+	return cfg
+}
