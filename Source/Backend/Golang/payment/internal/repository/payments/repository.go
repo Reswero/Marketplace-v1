cfb6d729eefa3ad5615efed7a017725af9e920a1
Reswero <reswero@mail.ru>
2025-03-31 00:13:48 +0600

    Добавлены методы в репозиторий для работы с отложенными и оплачеными платежами (Payment)

--- Source/Backend/Golang/payment/internal/repository/payments/repository.go
+++ Source/Backend/Golang/payment/internal/repository/payments/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/Masterminds/squirrel"
+	"github.com/Reswero/Marketplace-v1/payment/internal/domain/order"
 	"github.com/Reswero/Marketplace-v1/payment/internal/domain/payment"
 	"github.com/Reswero/Marketplace-v1/pkg/formatter"
 	"github.com/Reswero/Marketplace-v1/pkg/postgres"
@@ -44,6 +45,50 @@ func (r *Repository) AddPendingPayment(ctx context.Context, payment *payment.Pay
 	return nil
 }
 
+// Получить ожидающий платёж
+func (r *Repository) GetPendingPayment(ctx context.Context, orderId int64) (*payment.Payment, error) {
+	const op = "repository.payments.GetPendingPayment"
+
+	sql, args, err := r.Builder.Select("order_id, payment_amount, payment_id, valid_until").
+		From(pendingPaymentsTable).
+		Where(squirrel.Eq{"order_id": orderId}).
+		ToSql()
+	if err != nil {
+		return nil, formatter.FmtError(op, err)
+	}
+
+	row := r.Pool.QueryRow(ctx, sql, args...)
+
+	payment := &payment.Payment{
+		Order: &order.Order{},
+	}
+	err = row.Scan(&payment.Order.Id, &payment.Order.PaymentAmount,
+		&payment.Id, &payment.ValidUntil)
+	if err != nil {
+		return nil, formatter.FmtError(op, err)
+	}
+
+	return payment, nil
+}
+
+func (r *Repository) DeletePendingPayment(ctx context.Context, orderId int64) error {
+	const op = "repository.payments.DeletePendingPayment"
+
+	sql, args, err := r.Builder.Delete(pendingPaymentsTable).
+		Where(squirrel.Eq{"order_id": orderId}).
+		ToSql()
+	if err != nil {
+		return formatter.FmtError(op, err)
+	}
+
+	_, err = r.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return formatter.FmtError(op, err)
+	}
+
+	return nil
+}
+
 func (r *Repository) CheckPaymentIdExists(ctx context.Context, paymentId string) (bool, error) {
 	const op = "repository.payments.CheckPaymentIdExists"
 
@@ -62,4 +107,23 @@ func (r *Repository) CheckPaymentIdExists(ctx context.Context, paymentId string)
 	defer rows.Close()
 
 	return rows.Next(), nil
+}
+
+func (r *Repository) AddPaidPayment(ctx context.Context, payment *payment.Payment) error {
+	const op = "repository.payments.AddPaidPayment"
+
+	sql, args, err := r.Builder.Insert(paidPaymentsTable).
+		Columns("order_id, payment_amount, paid_at").
+		Values(payment.Order.Id, payment.Order.PaymentAmount, payment.PaidAt).
+		ToSql()
+	if err != nil {
+		return formatter.FmtError(op, err)
+	}
+
+	_, err = r.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return formatter.FmtError(op, err)
+	}
+
+	return nil
 }
--- Source/Backend/Golang/payment/internal/usecase/adapters/repository/interface.go
+++ Source/Backend/Golang/payment/internal/usecase/adapters/repository/interface.go
@@ -8,5 +8,8 @@ import (
 
 type Payments interface {
 	AddPendingPayment(ctx context.Context, payment *payment.Payment) error
+	GetPendingPayment(ctx context.Context, orderId int64) (*payment.Payment, error)
+	DeletePendingPayment(ctx context.Context, orderId int64) error
 	CheckPaymentIdExists(ctx context.Context, paymentId string) (bool, error)
+	AddPaidPayment(ctx context.Context, payment *payment.Payment) error
 }
