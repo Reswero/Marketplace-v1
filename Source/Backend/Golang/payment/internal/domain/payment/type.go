4699d88cfff894d73ad15e467f76792af177e42b
Reswero <reswero@mail.ru>
2025-03-30 18:08:51 +0600

    Добавлены доменные сущности (Payment)

---
+++ Source/Backend/Golang/payment/internal/domain/payment/type.go
@@ -0,0 +1,25 @@
+package payment
+
+import (
+	"time"
+
+	"github.com/Reswero/Marketplace-v1/payment/internal/domain/order"
+)
+
+// Оплата
+type Payment struct {
+	// Заказ
+	Order order.Order
+	// Идентфикатор
+	Id string
+	// Действительно до
+	ValidUntil time.Time
+}
+
+func New(order order.Order, id string) *Payment {
+	return &Payment{
+		Order:      order,
+		Id:         id,
+		ValidUntil: time.Now().Add(time.Minute * 30),
+	}
+}
