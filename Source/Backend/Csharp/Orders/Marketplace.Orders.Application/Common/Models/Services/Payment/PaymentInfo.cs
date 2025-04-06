6f2860caeeaadce8e058288423fe7c68a68f0271
Reswero <reswero@mail.ru>
2025-04-06 16:13:40 +0600

    Добавлен интерфейс для взаимодействия с сервисом Payment (Orders)

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Application/Common/Models/Services/Payment/PaymentInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Marketplace.Orders.Application.Common.Models.Services.Payment;
+
+/// <summary>
+/// Информация о платеже
+/// </summary>
+public class PaymentInfo(long orderId, int totalPrice)
+{
+    /// <summary>
+    /// Идентификатор заказа
+    /// </summary>
+    public long OrderId { get; private set; } = orderId;
+    /// <summary>
+    /// Итоговая цена
+    /// </summary>
+    public int TotalPrice { get; private set; } = totalPrice;
+}
