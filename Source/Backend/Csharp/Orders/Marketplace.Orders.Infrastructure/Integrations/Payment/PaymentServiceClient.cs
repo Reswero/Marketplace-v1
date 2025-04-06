bbbbbb525f39b7d43c19324eb1426ee0f829eaf9
Reswero <reswero@mail.ru>
2025-04-06 16:14:04 +0600

    Добавлена реализация клиента к сервису Payment (Orders)

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Infrastructure/Integrations/Payment/PaymentServiceClient.cs
@@ -0,0 +1,24 @@
+﻿using Marketplace.Orders.Application.Common.Interfaces;
+using Marketplace.Orders.Application.Common.Models.Services.Payment;
+using Marketplace.Payment.Grpc;
+
+namespace Marketplace.Orders.Infrastructure.Integrations.Payment;
+
+internal class PaymentServiceClient(PaymentService.PaymentServiceClient client)
+    : IPaymentServiceClient
+{
+    private readonly PaymentService.PaymentServiceClient _client = client;
+
+    /// <inheritdoc/>
+    public async Task<string> CreatePaymentAsync(PaymentInfo info, CancellationToken cancellationToken = default)
+    {
+        PaymentRequest request = new()
+        {
+            OrderId = info.OrderId,
+            PaybleAmount = info.TotalPrice
+        };
+
+        var response = await _client.CreatePaymentAsync(request, cancellationToken: cancellationToken);
+        return response.Id;
+    }
+}
