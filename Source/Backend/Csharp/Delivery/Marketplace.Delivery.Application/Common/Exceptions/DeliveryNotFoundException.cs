2670131b94bb66af483f8131854d3c3e425be38a
Reswero <reswero@mail.ru>
2025-04-12 21:35:35 +0600

    Добавлена реализация репозитория доставок заказов

---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Application/Common/Exceptions/DeliveryNotFoundException.cs
@@ -0,0 +1,5 @@
+﻿namespace Marketplace.Delivery.Application.Common.Exceptions;
+
+public class DeliveryNotFoundException(long orderId)
+    : Exception($"Не найдена доставка для заказа с идентификатором {orderId}")
+{ }
