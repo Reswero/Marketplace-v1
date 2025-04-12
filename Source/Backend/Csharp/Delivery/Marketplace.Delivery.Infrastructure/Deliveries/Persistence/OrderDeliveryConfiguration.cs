9bf3384c2774c9a27e4affaeb2b2a817c9f7fb42
Reswero <reswero@mail.ru>
2025-04-12 21:27:41 +0600

    Добавлены конфигурации для доменных сущностей (Delivery)

---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Infrastructure/Deliveries/Persistence/OrderDeliveryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Marketplace.Delivery.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Marketplace.Delivery.Infrastructure.Deliveries.Persistence;
+
+internal class OrderDeliveryConfiguration : IEntityTypeConfiguration<OrderDelivery>
+{
+    public void Configure(EntityTypeBuilder<OrderDelivery> builder)
+    {
+        builder.HasMany(d => d.Statuses)
+            .WithOne(s => s.OrderDelivery)
+            .HasForeignKey(s => s.OrderDeliveryId);
+    }
+}
---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Infrastructure/Deliveries/Persistence/OrderDeliveryStatusConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Marketplace.Delivery.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Marketplace.Delivery.Infrastructure.Deliveries.Persistence;
+
+internal class OrderDeliveryStatusConfiguration : IEntityTypeConfiguration<OrderDeliveryStatus>
+{
+    public void Configure(EntityTypeBuilder<OrderDeliveryStatus> builder)
+    {
+        builder.HasKey(s => new { s.OrderDeliveryId, s.Type });
+    }
+}
