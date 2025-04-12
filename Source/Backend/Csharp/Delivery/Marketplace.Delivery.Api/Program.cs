2c5575c7585fcf108bac4d3be870ff3635ce3ec0
Reswero <reswero@mail.ru>
2025-04-12 23:31:56 +0600

    Сконфигурирован запуск приложения (Delivery)

--- Source/Backend/Csharp/Delivery/Marketplace.Delivery.Api/Program.cs
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Api/Program.cs
@@ -1,4 +1,5 @@
-using Marketplace.Delivery.Api.Services;
+using Marketplace.Delivery.Infrastructure;
+using Marketplace.Delivery.Infrastructure.Deliveries.Services;
 
 namespace Marketplace.Delivery.Api;
 public class Program
@@ -7,14 +8,13 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddGrpc();
+        builder.Services.AddInfrastructure(builder.Configuration);
 
         var app = builder.Build();
+        app.Services.InitializeDatabase();
 
-        // Configure the HTTP request pipeline.
-        app.MapGrpcService<GreeterService>();
-        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+        app.MapGrpcService<DeliveryGrpcService>();
 
         app.Run();
     }
