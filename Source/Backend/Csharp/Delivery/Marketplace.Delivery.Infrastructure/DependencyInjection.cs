5b3a8b13c0ec1aacbf91338c33edfc24faa7e3f9
Reswero <reswero@mail.ru>
2025-04-12 23:10:28 +0600

    Добавлен класс для инъекций зависимостей инфраструктурного слоя (Delivery)

---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using Marketplace.Common.Transactions;
+using Marketplace.Delivery.Application.Common.Interfaces;
+using Marketplace.Delivery.Infrastructure.Common.Persistence;
+using Marketplace.Delivery.Infrastructure.Deliveries.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Marketplace.Delivery.Infrastructure;
+
+/// <summary>
+/// Внедрение зависимостей слоя инфраструктуры
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Добавить слой инфраструктуры
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration">Конфигурация</param>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabase(configuration);
+        services.AddRepositories();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextFactory<DeliveriesContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseNpgsql(connectionString);
+        });
+        services.AddScoped<IUnitOfWork, DeliveriesContext>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IOrderDeliveriesRepository, OrderDeliveriesRepository>();
+        return services;
+    }
+}
