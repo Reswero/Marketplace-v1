03f9bfd92bedcb62f2f46450128d71a8daf767d3
Reswero <reswero@mail.ru>
2025-04-13 13:22:41 +0600

    Добавлен интерфейс для grpc сервиса заказов (Delivery)

---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Application/Common/Interfaces/IOrdersServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using Marketplace.Delivery.Domain;
+
+namespace Marketplace.Delivery.Application.Common.Interfaces;
+
+/// <summary>
+/// Клиент к сервису заказов
+/// </summary>
+public interface IOrdersServiceClient
+{
+    /// <summary>
+    /// Изменить статус доставки заказа
+    /// </summary>
+    /// <param name="status">Статус</param>
+    /// <param name="cancellationToken"></param>
+    Task ChangeDeliveryStatusAsync(NewDeliveryStatus status, CancellationToken cancellationToken = default);
+}
