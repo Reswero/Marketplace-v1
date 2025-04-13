0f54e439112c5f6c486df028a69391ba243b6808
Reswero <reswero@mail.ru>
2025-04-13 14:57:09 +0600

    Добавлен клиент к сервису заказов с outbox'ом (Delivery)

---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Application/Common/Interfaces/IOrdersServiceClientOutboxed.cs
@@ -0,0 +1,16 @@
+﻿using Marketplace.Delivery.Domain;
+
+namespace Marketplace.Delivery.Application.Common.Interfaces;
+
+/// <summary>
+/// Клиент к сервису заказов с outbox'ом
+/// </summary>
+public interface IOrdersServiceClientOutboxed
+{
+    /// <summary>
+    /// Изменить статусы доставок заказов
+    /// </summary>
+    /// <param name="statuses">Статусы</param>
+    /// <param name="cancellationToken"></param>
+    Task ChangeDeliveryStatusesAsync(IReadOnlyList<NewDeliveryStatus> statuses, CancellationToken cancellationToken = default);
+}
---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Infrastructure/Integrations/Orders/OrdersServiceClientOutboxed.cs
@@ -0,0 +1,38 @@
+﻿using Marketplace.Common.Outbox.Queue;
+using Marketplace.Delivery.Application.Common.Interfaces;
+using Marketplace.Delivery.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace Marketplace.Delivery.Infrastructure.Integrations.Orders;
+
+/// <summary>
+/// Клиент к сервису заказов с outbox'ом
+/// </summary>
+internal class OrdersServiceClientOutboxed(ILogger<OrdersServiceClientOutboxed> logger,
+    IOrdersServiceClient client, IOutboxQueue<NewDeliveryStatus> outbox)
+    : IOrdersServiceClientOutboxed
+{
+    private readonly ILogger<OrdersServiceClientOutboxed> _logger = logger;
+    private readonly IOrdersServiceClient _client = client;
+    private readonly IOutboxQueue<NewDeliveryStatus> _outbox = outbox;
+
+    /// <inheritdoc/>
+    public async Task ChangeDeliveryStatusesAsync(IReadOnlyList<NewDeliveryStatus> statuses, CancellationToken cancellationToken = default)
+    {
+        List<NewDeliveryStatus> notSendedStatuses = [];
+        foreach (var status in statuses)
+        {
+            try
+            {
+                await _client.ChangeDeliveryStatusAsync(status, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                notSendedStatuses.Add(status);
+                _logger.LogError(e, "Произошла ошибка во время отправки статуса доставки. {Error}", e.Message);
+            }
+        }
+
+        await _outbox.PushAsync([.. notSendedStatuses], cancellationToken);
+    }
+}
