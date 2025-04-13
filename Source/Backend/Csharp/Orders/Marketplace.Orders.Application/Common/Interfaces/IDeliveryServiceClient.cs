91a9e5c7bb45ad74c1b91f73deff507cb05860c4
Reswero <reswero@mail.ru>
2025-04-13 19:24:06 +0600

    Добавлен клиент к сервису доставки (Orders)

---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Application/Common/Interfaces/IDeliveryServiceClient.cs
@@ -0,0 +1,20 @@
+﻿namespace Marketplace.Orders.Application.Common.Interfaces;
+
+/// <summary>
+/// Клиент к сервису доставки
+/// </summary>
+public interface IDeliveryServiceClient
+{
+    /// <summary>
+    /// Создать доставку заказа
+    /// </summary>
+    /// <param name="orderId">Идентификатор заказа</param>
+    /// <param name="cancellationToken"></param>
+    public Task CreateDeliveryAsync(int orderId, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Отменить доставку заказа
+    /// </summary>
+    /// <param name="orderId">Идентификатор заказа</param>
+    /// <param name="cancellationToken"></param>
+    public Task CancelDeliveryAsync(int orderId, CancellationToken cancellationToken = default);
+}
---
+++ Source/Backend/Csharp/Orders/Marketplace.Orders.Infrastructure/Integrations/Delivery/Services/DeliveryServiceClient.cs
@@ -0,0 +1,27 @@
+﻿using Marketplace.Delivery.Server;
+using Marketplace.Orders.Application.Common.Interfaces;
+
+namespace Marketplace.Orders.Infrastructure.Integrations.Delivery.Services;
+
+/// <summary>
+/// Клиент к сервису доставки
+/// </summary>
+/// <param name="client"></param>
+internal class DeliveryServiceClient(DeliveryService.DeliveryServiceClient client) : IDeliveryServiceClient
+{
+    private readonly DeliveryService.DeliveryServiceClient _client = client;
+
+    /// <inheritdoc/>
+    public async Task CreateDeliveryAsync(int orderId, CancellationToken cancellationToken = default)
+    {
+        CreateDeliveryRequest request = new() { OrderId = orderId };
+        await _client.CreateDeliveryAsync(request, cancellationToken: cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task CancelDeliveryAsync(int orderId, CancellationToken cancellationToken = default)
+    {
+        CancelDeliveryRequest request = new() { OrderId = orderId };
+        await _client.CancelDeliveryAsync(request, cancellationToken: cancellationToken);
+    }
+}
