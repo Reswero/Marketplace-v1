683e66f7b3207f55110e7decbb6cb6ee8e3ee851
Reswero <reswero@mail.ru>
2025-04-13 13:21:53 +0600

    Добавлена сущность NewDeliveryStatus (Delivery)

---
+++ Source/Backend/Csharp/Delivery/Marketplace.Delivery.Domain/NewDeliveryStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace Marketplace.Delivery.Domain;
+
+/// <summary>
+/// Новый статус доставки
+/// </summary>
+public class NewDeliveryStatus
+{
+    /// <summary>
+    /// Идентификатор заказа
+    /// </summary>
+    public long OrderId { get; private set; }
+    /// <summary>
+    /// Тип статуса
+    /// </summary>
+    public OrderDeliveryStatusType Type { get; private set; }
+    /// <summary>
+    /// Произошло в
+    /// </summary>
+    public DateTimeOffset OccuredAt { get; private set; }
+}
